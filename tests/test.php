af25c309b93c58d60fee08c42bb128c6403f9f2a
encircles <encircles@163.com>
2019-01-25 16:14:46 +0800

    eeeee

--- tests/test.php
+++ tests/test.php
@@ -7,3 +7,21 @@
  */
 
 require_once '../vendor/autoload.php';
+
+try {
+    $snow = new \Encircles\SnowFlake\SnowFlake(1, 1);
+
+    $conn = new \Redis();
+    $conn->connect('127.0.0.1', 16379);
+
+    for ($i = 0; $i < 100000; $i++) {
+        $id = $snow->generateID();
+        $conn->lPush('SNOW_ID', $id);
+    }
+
+    echo 'ok' . PHP_EOL;
+
+//    $conn->
+} catch (Exception $e) {
+
+}
